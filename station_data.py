93b4fd48271811eefd251461fe1ab47ba1521431
Slesa Adhikari <slesaad@gmail.com>
2023-09-05 16:13:36 -0500

    Add flask data and update code

--- station_data.py
+++ station_data.py
@@ -1,19 +1,21 @@
 import os
+import sys
 
-files = os.listdir()
+base_dir = sys.argv[1]
+files = os.listdir(base_dir)
 stations = []
 initial_line = 30
-final_line = 37
+final_line = 50
 for file in files:
     if not file.endswith(".txt"):
         continue
-    with open(file) as ch4_file:
+    with open(f"{base_dir}/{file}") as ch4_file:
         data = ch4_file.readlines()
         relevant_data = data[initial_line:final_line]
         station = {}
         for datum in relevant_data:
             key, value = datum.strip(" ").strip("#").strip("\n").split(" : ")
-            station[key] = value
+            station[key.strip(" ")] = value
         station["csvFiles"] = file
         stations.append(station)
 
