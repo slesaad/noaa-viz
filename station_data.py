77cfef37fbe58ab0e7c47e5ea45b48f9cf586aa3
Slesa Adhikari <slesaad@gmail.com>
2023-09-11 16:54:48 -0500

    Update station generation code

--- station_data.py
+++ station_data.py
@@ -11,12 +11,19 @@ for file in files:
         continue
     with open(f"{base_dir}/{file}") as ch4_file:
         data = ch4_file.readlines()
+        header_line = data[0].split(" : ")[-1]
         relevant_data = data[initial_line:final_line]
-        station = {}
-        for datum in relevant_data:
-            key, value = datum.strip(" ").strip("#").strip("\n").split(" : ")
-            station[key.strip(" ")] = value
-        station["csvFiles"] = file
-        stations.append(station)
+        years = set()
+        for line in data[int(header_line) :]:
+            year = line.split(" ")[1]
+            years.add(year)
+        if len(years) > 1:
+            station = {}
+            for datum in relevant_data:
+                key, value = datum.strip(" ").strip("#").strip("\n").split(" : ")
+                station[key.strip(" ")] = value
+            stations.append(station)
+        else:
+            print(file)
 
 print(stations)
