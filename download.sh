9cac7fae478a1ba52e8990a19d523a4f84f2c869
Slesa Adhikari <slesaad@gmail.com>
2023-11-16 16:56:48 -0600

    Also download surface-pfp data files

--- download.sh
+++ download.sh
@@ -1,11 +1,14 @@
 #If something fails with exit!=0 the script stops
 set -e
 
-# Download co2 surface flask zip
+# Download files
 wget https://gml.noaa.gov/aftp/data/trace_gases/co2/flask/surface/co2_surface-flask_ccgg_text.zip
-# Download ch4 surface flask zip
 wget https://gml.noaa.gov/aftp/data/trace_gases/ch4/flask/surface/ch4_surface-flask_ccgg_text.zip
+wget https://gml.noaa.gov/aftp/data/trace_gases/ch4/pfp/surface/ch4_surface-pfp_ccgg_text.zip
+wget https://gml.noaa.gov/aftp/data/trace_gases/co2/pfp/surface/co2_surface-pfp_ccgg_text.zip
 
-# unzip to data folder
+# unzip to respective folders
 unzip -j -o co2_surface-flask_ccgg_text.zip co2_surface-flask_ccgg_text/*_event.txt -d flask/co2
 unzip -j -o ch4_surface-flask_ccgg_text.zip ch4_surface-flask_ccgg_text/*_event.txt -d flask/ch4
+unzip -j -o co2_surface-pfp_ccgg_text.zip co2_surface-pfp_ccgg_text/*_event.txt -d surface-pfp/co2
+unzip -j -o ch4_surface-pfp_ccgg_text.zip ch4_surface-pfp_ccgg_text/*_event.txt -d surface-pfp/ch4
